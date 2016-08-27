d7d0a2129c502bd0ac78ad2247bfece13cd2428a
ad34 <nraby@access-dev.com>
2016-08-27 15:31:05 +0200

    build script for x86_64/arm64
    build script for x86_64/arm64

---
+++ build-arm64_so.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+./build-so.sh /Developer/android-ndk-r12b \
+                /Users/erwan/Documents/OtherProjects/openSSLBuild/openssl-1.0.2h \
+                23 \
+                arm64-v8a \
+                4.8 \
+                /Users/erwan/output/arm64-v8a
---
+++ build-x86_64_so.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+./build-so.sh /Developer/android-ndk-r12b \
+                /Users/erwan/Documents/OtherProjects/openSSLBuild/openssl-1.0.2h \
+                23 \
+                x86_64 \
+                4.8 \
+                /Users/erwan/output/x86_64
