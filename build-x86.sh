bf3996e6f389f8ae15fca33a1a87b46358ed78ba
ad34 <nraby@access-dev.com>
2016-08-27 11:56:56 +0200

    add build script for armv7

--- build-x86.sh
+++ build-x86.sh
@@ -2,18 +2,17 @@
 
 rm -rf openssl-1.0.2h/
 tar xzf openssl-1.0.2h.tar.gz
-chmod a+x setenv-android-arm64.sh
+chmod a+x setenv-android-x86.sh
 
 . ./setenv-android-x86.sh
 
 cd openssl-1.0.2h/
 
 perl -pi -e 's/install: all install_docs install_sw/install: install_docs install_sw/g' Makefile.org
-./config shared no-ssl2 no-ssl3 no-hw  --openssldir=/usr/local/ssl/android19
+./config shared no-ssl2 no-ssl3 no-hw  --openssldir=/usr/local/ssl/android19/x86
 
 make clean
 make depend
 make all
 
 sudo -E make install CC=$ANDROID_TOOLCHAIN/i686-linux-android-gcc RANLIB=$ANDROID_TOOLCHAIN/i686-linux-android-gcc-ranlib
-
